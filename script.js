324eb529fd88c7767c4bc856868a3785a4e100cb
1morningcoffee <110513082+1morningcoffee@users.noreply.github.com>
2024-06-18 09:20:32 -0400

    Created 16x16 grid of div squares

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const container = document.querySelector("#container");
 
 for (let i = 0; i <  16; i++) {
-    const square = document.createElement('div');
-    square.className = 'grid';
-    document.querySelector('#container').appendChild(square);
+    const column = document.createElement('div');
+    column.className = 'column';
+    for (let j = 0; j < 16; j++) {
+        const square = document.createElement('div');
+        square.className = 'grid';
+    
+    column.appendChild(square);
+    }
+    
+    container.appendChild(column);
 }
 
 container.appendChild(square);
