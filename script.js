41e05c56e3960df224e66208ac473a5e74582929
1morningcoffee <110513082+1morningcoffee@users.noreply.github.com>
2024-06-18 08:54:49 -0400

    Created multiple divs in the html div container

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// no code yet
+const container = document.querySelector("#container");
+
+for (let i = 0; i <  16; i++) {
+    const square = document.createElement('div');
+    square.className = 'grid';
+    document.querySelector('#container').appendChild(square);
+}
+
+container.appendChild(square);
