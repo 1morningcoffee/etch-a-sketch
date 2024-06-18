af8d9fad359bed737f44298257bf4d2dee59a36f
1morningcoffee <110513082+1morningcoffee@users.noreply.github.com>
2024-06-18 11:11:11 -0400

    Added background change on mouseover

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ for (let i = 0; i <  16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement('div');
         square.className = 'grid';
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'black';
+        })
     
     column.appendChild(square);
     }
